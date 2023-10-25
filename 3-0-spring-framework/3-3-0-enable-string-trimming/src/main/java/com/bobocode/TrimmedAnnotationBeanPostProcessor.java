f41fbaac0add2c68c85cf350d04ddef2ecfaacf8
RomanPryshchepa <roman.pryshchepa@gmail.com>
2023-10-25 17:17:51 +0200

    solved 3-3-0-enable-string-trimming (Home Training 16)

--- 3-0-spring-framework/3-3-0-enable-string-trimming/src/main/java/com/bobocode/TrimmedAnnotationBeanPostProcessor.java
+++ 3-0-spring-framework/3-3-0-enable-string-trimming/src/main/java/com/bobocode/TrimmedAnnotationBeanPostProcessor.java
@@ -1,9 +1,18 @@
 package com.bobocode;
 
+import com.bobocode.annotation.EnableStringTrimming;
 import com.bobocode.annotation.Trimmed;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNullApi;
 import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
 
 /**
  * This is processor class implements {@link BeanPostProcessor}, looks for a beans where method parameters are marked with
@@ -11,11 +20,34 @@ import org.springframework.stereotype.Component;
  * {@link Trimmed}. For example if there is a string " Java   " as an input parameter it has to be automatically trimmed to "Java"
  * if parameter is marked with {@link Trimmed} annotation.
  * <p>
- *
+ * <p>
  * Note! This bean is not marked as a {@link Component} to avoid automatic scanning, instead it should be created in
  * {@link StringTrimmingConfiguration} class which can be imported to a {@link Configuration} class by annotation
  * {@link EnableStringTrimming}
  */
-public class TrimmedAnnotationBeanPostProcessor {
-//todo: Implement TrimmedAnnotationBeanPostProcessor according to javadoc
+public class TrimmedAnnotationBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        boolean present = Arrays.stream(bean.getClass().getDeclaredMethods())
+                .flatMap(method -> Arrays.stream(method.getParameters()))
+                .filter(parameter -> parameter.isAnnotationPresent(Trimmed.class))
+                .anyMatch(parameter -> parameter.getType().equals(String.class));
+        if (present) {
+            MethodInterceptor methodInterceptor = (obj, method, args, proxy) -> {
+                Parameter[] params = method.getParameters();
+                for (int k = 0; k < params.length; k++) {
+                    for (Annotation annotation : params[k].getDeclaredAnnotations()) {
+                        if (params[k].getType().equals(String.class)
+                                && "Trimmed".equals(annotation.annotationType().getSimpleName())) {
+                            args[k] = ((String) args[k]).trim();
+                        }
+                    }
+                }
+                return proxy.invokeSuper(obj, args);
+            };
+            return Enhancer.create(bean.getClass(), methodInterceptor);
+
+        }
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
 }
